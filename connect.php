40fc789f36c124cc97723db948ce67f5aad84698
TranNgocMinhThong <tranthng207@gmail.com>
2021-11-10 16:32:16 +0700

    add connect.php

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+$username = "ptud"; // Khai báo username
+$password = "ptud";      // Khai báo password
+$server   = "localhost";   // Khai báo server
+$dbname   = "ptudweb";      // Khai báo database
+
+// Kết nối database tintuc
+$connect = mysqli_connect($server, $username, $password, $dbname);
+
+//Nếu kết nối bị lỗi thì xuất báo lỗi và thoát.
+if (!$connect) {
+    die("Không kết nối :" . mysqli_connect_error());
+    exit();
+}
+echo "Khi kết nối thành công sẽ tiếp tục dòng code bên dưới đây."
+?>
